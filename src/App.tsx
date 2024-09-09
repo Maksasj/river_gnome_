d870a061bbeef976cb22aa3d6d79accf38323b02
Maksim Jaroslavcevas <36482401+Maksasj@users.noreply.github.com>
2024-09-09 14:45:37 +0300

    fixed build issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Instagram, Pin, X } from 'lucide-react'
+import { Instagram, X } from 'lucide-react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div className="card">
